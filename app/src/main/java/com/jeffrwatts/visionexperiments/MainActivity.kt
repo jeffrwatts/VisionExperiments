152549007f4d24097b05018cb39102a4fd08a7e9
Jeff Watts <jwatts@netflix.com>
2022-03-05 07:25:06 -0800

    Camera Permission

--- app/src/main/java/com/jeffrwatts/visionexperiments/MainActivity.kt
+++ app/src/main/java/com/jeffrwatts/visionexperiments/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.jeffrwatts.visionexperiments
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 1
+    }
+    private val buttonBasicCamera: Button by lazy { findViewById(R.id.buttonBasicCamera) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonBasicCamera.isEnabled = false
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_REQUEST_CODE)
+        } else {
+            onCameraPermissionsGranted()
+        }
+    }
+
+    private fun onCameraPermissionsGranted() {
+        buttonBasicCamera.isEnabled = true
+    }
+
+    private fun launchActivity(klass: Class<*>) {
+        val intent = Intent(this, klass)
+        startActivity(intent)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+            onCameraPermissionsGranted()
+        } else {
+            Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_LONG).show()
+        }
     }
 }
